53493245913f07bf5fe8a924c44c78fbd405cc8f
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-09 00:05:25 +0900

    リダイレクト処理

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @photos = Photo.all
   end
@@ -14,6 +16,12 @@ class PhotosController < ApplicationController
   private
   def photo_params
     params.require(:photo).permit(:name, :image, :text)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end

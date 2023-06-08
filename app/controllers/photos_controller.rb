3e45dec5e7d92b98baa630aaa21224395a20d54e
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-08 14:58:41 +0900

    createアクション、ストロングパロメーターの定義

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,14 @@ class PhotosController < ApplicationController
   def new
     @photos = Photo.new
   end
+
+  def create
+    Photo.create(photo_params)
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:name, :image, :text)
+  end
+
 end
